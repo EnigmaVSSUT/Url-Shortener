16a2a5b4dfe1ef11b76f279ee4670b298c91f63c
sumanpatra006 <ksumanpatra06@gmail.com>
2025-05-29 09:36:32 +0530

    fronetend checks

--- FRONTEND/src/pages/AuthPage.jsx
+++ FRONTEND/src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import LoginForm from "../components/loginForm";
+import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
 
 const AuthPage = () => {
