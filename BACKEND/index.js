1c463ef898de943783d2381c9bbcb64a88ae70f8
sumanpatra006 <ksumanpatra06@gmail.com>
2025-06-02 17:00:43 +0530

    cors error fix

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://linksnap-six.vercel.app/", 
+      "https://linksnap-six.vercel.app", 
     ],
     credentials: true,
   })
